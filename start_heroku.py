dff045f888a5fd1e2610d0844776e7a14e5b816f
Igor <yudin-igor@rambler.ru>
2019-06-08 01:48:12 +0300

    Создание каркаса

--- start_heroku.py
+++ start_heroku.py
@@ -6,11 +6,12 @@ print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 app = Flask(__name__)
 string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
 
+DB = MongoDB(connection_string=string, flag_ssl=False)
+document = {'Hello BD': 'PriceCompareBot'}
+coll = DB.CLIENT['PriceCompareBot']['PriceCompareBot']
+coll.save(document)
+
 
 @app.route('/')
 def index():
-    DB = MongoDB(connection_string=string, flag_ssl=False)
-    document = {'Hello BD': 'PriceCompareBot'}
-    coll = DB.CLIENT['PriceCompareBot']['PriceCompareBot']
-    coll.save(document)
     return '@@@@@@@@@@@@@@ Hello Web PriceCompareBot @@@@@@@@@@@@@@'
