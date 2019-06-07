80de327d7f8916288b244837e0d078e1e03b86cf
Igor <yudin-igor@rambler.ru>
2019-06-08 00:52:33 +0300

    Создание каркаса

--- start_pycharm.py
+++ start_pycharm.py
@@ -1,4 +1,19 @@
-import start_heroku
+from flask import Flask
+from ClassLawr_MongoDB import MongoDB
+from ClassLawr_BotWebhook import BotWebhook
+
+print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
+app = Flask(__name__)
+string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
+DB = MongoDB(connection_string=string, flag_ssl=True)
+
+
+@app.route('/')
+def index():
+    data = {'Hello BD': 'PriceCompareBot'}
+    DB.add_line_to_end(dbs='PriceCompareBot', collection='PriceCompareBot', value=data)
+    return '@@@@@@@@@@@@@@ Hello Web PriceCompareBot @@@@@@@@@@@@@@'
+
 
 if __name__ == '__main__':
-    start_heroku.app.run()
+    app.run()
