d201ec8fca80a18c22e591c77bf7a0eda9ba053c
Igor <yudin-igor@rambler.ru>
2019-06-08 01:53:28 +0300

    Создание каркаса

--- start_heroku.py
+++ start_heroku.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pymongo
 from ClassLawr_MongoDB import MongoDB
 from ClassLawr_BotWebhook import BotWebhook
 
@@ -6,7 +7,7 @@ print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 app = Flask(__name__)
 string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
 
-DB = MongoDB(connection_string=string, flag_ssl=False)
+DB = pymongo.MongoClient(string)
 document = {'Hello BD': 'PriceCompareBot'}
 coll = DB.CLIENT['PriceCompareBot']['PriceCompareBot']
 coll.save(document)
