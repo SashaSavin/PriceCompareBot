0db62a65285e56b8aefd30d06f4e4b4c0dc4efd1
Igor <yudin-igor@rambler.ru>
2019-06-08 20:43:37 +0300

    Создание каркаса

--- start_heroku.py
+++ start_heroku.py
@@ -1,11 +1,12 @@
 from flask import Flask
-import pymongo
+# import pymongo
 from ClassLawr_MongoDB import MongoDB
 from ClassLawr_BotWebhook import BotWebhook
 
 app = Flask(__name__)
 string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
 DB = MongoDB(connection_string=string, flag_ssl=True)
+print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 
 
 @app.route('/')
--- start_pycharm.py
+++ start_pycharm.py
@@ -2,10 +2,10 @@ from flask import Flask
 from ClassLawr_MongoDB import MongoDB
 from ClassLawr_BotWebhook import BotWebhook
 
-print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 app = Flask(__name__)
 string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
 DB = MongoDB(connection_string=string, flag_ssl=True)
+print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 
 
 @app.route('/')
