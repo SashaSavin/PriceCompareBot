d7b7fe4ac422725e3a3835bd5541726f6c70656c
Igor <yudin-igor@rambler.ru>
2019-06-08 18:01:56 +0300

    Создание каркаса

--- start_heroku.py
+++ start_heroku.py
@@ -5,7 +5,6 @@ from ClassLawr_BotWebhook import BotWebhook
 
 app = Flask(__name__)
 string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
-
 DB = pymongo.MongoClient(string)
 document = {'Hello BD': 'PriceCompareBot'}
 coll = DB.CLIENT['PriceCompareBot']['PriceCompareBot']
@@ -15,4 +14,9 @@ print('@@@@@@@@@@@@@@ Hello Console PriceCompareBot @@@@@@@@@@@@@@')
 
 @app.route('/')
 def index():
+    string = "mongodb+srv://Admin:HIUI#H*H89@pricecomparebot-mhmiv.mongodb.net/test?retryWrites=true&w=majority"
+    DB = pymongo.MongoClient(string)
+    document = {'Hello BD': 'PriceCompareBot'}
+    coll = DB.CLIENT['PriceCompareBot']['PriceCompareBot']
+    coll.save(document)
     return '@@@@@@@@@@@@@@ Hello Web PriceCompareBot @@@@@@@@@@@@@@'
